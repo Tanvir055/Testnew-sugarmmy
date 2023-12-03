d9827a320613fbc5f78b292e7dce957571ee4102
Tanvir055 <90306629+Tanvir055@users.noreply.github.com>
2023-12-03 11:43:46 +0600

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,15 @@ OWNER_ID = int(getenv("OWNER_ID", 1781037279))
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/subinps/VCPlayerBot",
+)
 
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+GIT_TOKEN = getenv(
+    "GIT_TOKEN", None
+)
 # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/feelingspeace")
