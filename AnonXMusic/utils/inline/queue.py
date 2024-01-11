17d55c0286c742eabec61c3451b1c3a18be41572
Tanvir055 <90306629+Tanvir055@users.noreply.github.com>
2024-01-11 14:19:46 +0600

    Update queue.py

--- AnonXMusic/utils/inline/queue.py
+++ AnonXMusic/utils/inline/queue.py
@@ -69,7 +69,7 @@ def aq_markup(_, chat_id):
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="🗑️", callback_data=f"ADMIN forceclose|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
     ]
     return buttons
