c1f75abe3af8a01fb6c0db6185f7d124c5fb5114
Tanvir400216 <122742311+Tanvir400216@users.noreply.github.com>
2024-03-09 11:36:41 +0600

    Update couples.py

--- AnonXMusic/plugins/tools/couples.py
+++ AnonXMusic/plugins/tools/couples.py
@@ -7,137 +7,25 @@ from pyrogram import *
 from pyrogram.types import *
 from pyrogram.enums import *
 
+Certainly! If you're looking for a more comprehensive Python script with some functionality, here's an example of a basic "couple" program that takes user input for two names and prints a message:
 
+```python
+# couple.py
 
-def dt():
-    now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M")
-    dt_list = dt_string.split(" ")
-    return dt_list
+def main():
+    print("Welcome to the Couple Program!")
     
-
-def dt_tom():
-    a = (
-        str(int(dt()[0].split("/")[0]) + 1)
-        + "/"
-        + dt()[0].split("/")[1]
-        + "/"
-        + dt()[0].split("/")[2]
-    )
-    return a
-
-tomorrow = str(dt_tom())
-today = str(dt()[0])
-
-
-async def ctest(_, message):
-    cid = message.chat.id
-    if message.chat.type == ChatType.PRIVATE:
-        return await message.reply_text("This command only works in groups.")
-    try:
-     #  is_selected = await get_couple(cid, today)
-     #  if not is_selected:
-         msg = await message.reply_text("ɢᴇɴᴇʀᴀᴛɪɴɢ ᴄᴏᴜᴘʟᴇꜱ ɪᴍᴀɢᴇ...")
-         #GET LIST OF USERS
-         list_of_users = []
-
-         async for i in app.get_chat_members(message.chat.id, limit=50):
-             if not i.user.is_bot:
-               list_of_users.append(i.user.id)
-
-         c1_id = random.choice(list_of_users)
-         c2_id = random.choice(list_of_users)
-         while c1_id == c2_id:
-              c1_id = random.choice(list_of_users)
-
-
-         photo1 = (await app.get_chat(c1_id)).photo
-         photo2 = (await app.get_chat(c2_id)).photo
- 
-         N1 = (await app.get_users(c1_id)).mention 
-         N2 = (await app.get_users(c2_id)).mention
-         
-         try:
-            p1 = await app.download_media(photo1.big_file_id, file_name="pfp.png")
-         except Exception:
-            p1 = "BrandrdXMusic/assets/upic.png"
-         try:
-            p2 = await app.download_media(photo2.big_file_id, file_name="pfp1.png")
-         except Exception:
-            p2 = "BrandrdXMusic/assets/upic.png"
-            
-         img1 = Image.open(f"{p1}")
-         img2 = Image.open(f"{p2}")
-
-         img = Image.open("BrandrdXMusic/assets/cppicbranded.jpg")
-
-         img1 = img1.resize((437,437))
-         img2 = img2.resize((437,437))
-
-         mask = Image.new('L', img1.size, 0)
-         draw = ImageDraw.Draw(mask) 
-         draw.ellipse((0, 0) + img1.size, fill=255)
-
-         mask1 = Image.new('L', img2.size, 0)
-         draw = ImageDraw.Draw(mask1) 
-         draw.ellipse((0, 0) + img2.size, fill=255)
-
-
-         img1.putalpha(mask)
-         img2.putalpha(mask1)
-
-         draw = ImageDraw.Draw(img)
-
-         img.paste(img1, (116, 160), img1)
-         img.paste(img2, (789, 160), img2)
-
-         img.save(f'test_{cid}.png')
+    # Get names from the user
+    name1 = input("Enter the first person's name: ")
+    name2 = input("Enter the second person's name: ")
     
-         TXT = f"""
-**𝐓𝐎𝐃𝐀𝐘'𝐒 𝐒𝐄𝐋𝐄𝐂𝐓𝐄𝐃 𝐂𝐎𝐔𝐏𝐋𝐄𝐒 🎉 :
-➖➖➖➖➖➖➖➖➖➖➖➖
-{N1} + {N2} = ❣️
-➖➖➖➖➖➖➖➖➖➖➖➖
-𝐍𝐄𝐗𝐓 𝐂𝐎𝐔𝐏𝐋𝐄𝐒 𝐖𝐈𝐋𝐋 𝐁𝐄 𝐒𝐄𝐋𝐄𝐂𝐓𝐄𝐃 𝐎𝐍 {tomorrow} !!**
-"""
-    
-         await message.reply_photo(f"test_{cid}.png", caption=TXT)
-         await msg.delete()
-         a = upload_file(f"test_{cid}.png")
-         for x in a:
-           img = "https://graph.org/" + x
-           couple = {"c1_id": c1_id, "c2_id": c2_id}
-          # await save_couple(cid, today, couple, img)
-    
-         
-      # elif is_selected:
-      #   msg = await message.reply_text("𝗚𝗘𝗧𝗧𝗜𝗡𝗚 𝐓𝐎𝐃𝐀𝐘'𝐒 𝐂𝐎𝐔𝐏𝐋𝐄𝐒 𝐈𝐌𝐀𝐆𝐄...")
-      #   b = await _get_image(cid)
-       #  c1_id = int(is_selected["c1_id"])
-       #  c2_id = int(is_selected["c2_id"])
-       #  c1_name = (await app.get_users(c1_id)).first_name
-        # c2_name = (await app.get_users(c2_id)).first_name
-         
-      #   TXT = f"""
-#**𝐓𝐎𝐃𝐀𝐘'𝐒 𝐒𝐄𝐋𝐄𝐂𝐓𝐄𝐃 𝐂𝐎𝐔𝐏𝐋𝐄𝐒 🎉 :
-#➖➖➖➖➖➖➖➖➖➖➖➖
-#[{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❣️
-#➖➖➖➖➖➖➖➖➖➖➖➖
-#𝐍𝐄𝐗𝐓 𝐂𝐎𝐔𝐏𝐋𝐄𝐒 𝐖𝐈𝐋𝐋 𝐁𝐄 𝐒𝐄𝐋𝐄𝐂𝐓𝐄𝐃 𝐎𝐍 {tomorrow} !!**
-#"""
+    # Print a message
+    print(f"{name1} and {name2} make a great couple!")
 
- #        await message.reply_photo(b, caption=TXT)
-        # await msg.delete()
-    except Exception as e:
-        print(str(e))
-    try:
-      os.remove(f"./downloads/pfp1.png")
-      os.remove(f"./downloads/pfp2.png")
-      os.remove(f"test_{cid}.png")
-    except Exception:
-       pass
+if __name__ == "__main__":
+    main()
+```
 
-__mod__ = "COUPLES"
-__help__ = """
-**» /couples** - Get Todays Couples Of The Group In Interactive View
-"""
+This script prompts the user to enter the names of two people and then prints a message declaring them a great couple. You can run this script in a Python environment, and it will guide you through the process.
+
+Feel free to specify if you have any particular functionality or requirements in mind for the "couple" program!
