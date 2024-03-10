b3281f6427f14fbaec1820711289617f045cdbac
Tanvir400216 <122742311+Tanvir400216@users.noreply.github.com>
2024-03-10 12:51:50 +0600

    Update help.py

--- AnonXMusic/plugins/bot/help.py
+++ AnonXMusic/plugins/bot/help.py
@@ -38,19 +38,20 @@ async def helper_private(
         language = await get_lang(update.chat.id)
         _ = get_string(language)
         keyboard = help_pannel(_)
-        await update.reply_photo(
-            photo=START_IMG_URL,
-            caption=_["help_1"].format(SUPPORT_CHAT),
-            reply_markup=keyboard,
-        )
+
+        await update.reply_video(
+            video="https://graph.org/file/84d30d4fd04570c0e0256.mp4",
+            caption=_["help_1"].format(SUPPORT_CHAT), reply_markup=keyboard)
 
 
 @app.on_message(filters.command(["help"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
 async def help_com_group(client, message: Message, _):
     keyboard = private_help_panel(_)
-    await message.reply_text(_["help_2"], reply_markup=InlineKeyboardMarkup(keyboard))
-
+    await message.reply_video(
+        video="https://te.legra.ph/file/51293513e6af319726fe7.mp4",
+        caption=_["help_2"], reply_markup=InlineKeyboardMarkup(keyboard)
+    )
 
 @app.on_callback_query(filters.regex("help_callback") & ~BANNED_USERS)
 @languageCB
