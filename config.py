bed16c3ba2759875d866de0bca6b9b553aa9671e
Tanvir400216 <122742311+Tanvir400216@users.noreply.github.com>
2024-03-10 16:47:37 +0600

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/62c76ac2095332a0ede75.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/d9e2f565a6582d25403dc.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/00dcbad607e1423270044.jpg"
