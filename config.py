ea714f8f4c14e6d099cd1f1544308877634e7d82
Tanvir400216 <122742311+Tanvir400216@users.noreply.github.com>
2024-03-14 12:56:49 +0600

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
    
-   
+   "https://github.com/Tanvi555/Telethon-Music",
 )
 
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
