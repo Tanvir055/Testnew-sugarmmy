70ddc664a71661bfce34cc06d150d0627fdebf2a
MD TANVIR <122742311+Tanvir400216@users.noreply.github.com>
2024-08-18 09:21:16 +0600

    Update Youtube.py

--- AnonXMusic/platforms/Youtube.py
+++ AnonXMusic/platforms/Youtube.py
@@ -10,8 +10,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from BrandrdXMusic.utils.database import is_on_off
-from BrandrdXMusic.utils.formatters import time_to_seconds
+from AnonXMusic.utils.database import is_on_off
+from AnonXMusic.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
